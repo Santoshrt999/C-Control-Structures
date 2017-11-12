c985a7d039f160fe9c2365014216e4714da67703
Santosh Ravi Teja <S528130@MAIL.NWMISSOURI.EDU>
2017-11-12 01:28:52 -0600

    Add files via upload

---
+++ ControlStructures/ControlStructures/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ControlStructures
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isGold = true;
+            float price;
+            if (isGold)
+            {
+                price = 19.95f;
+            }
+            else {
+                price = 21.24f;
+            }
+             price = (isGold)? 19.95f : 21.24f;
+
+
+
+            var SAN = season.Autumn;
+
+            switch (SAN)
+            {
+                case season.Autumn:
+                    Console.WriteLine("very HOT");
+
+                    break;
+
+                case season.Spring:
+                    Console.WriteLine("very chilly");
+                    break;
+
+                case season.Winter:
+                    Console.WriteLine("very chilly");
+
+                    break;
+
+                case season.Fall:
+                    Console.WriteLine("very chilly");
+
+                    break;
+
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
